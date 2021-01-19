26eb631d9e65e575f229687c0772235ca4b0f001
dcoppage86 <dakotakcoppage@me.com>
2021-01-18 23:13:32 -0700

    used nested resource to make users the parent of user_breweries

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
 
-  resources :user_breweries, only: [:new, :create, :destroy, :show]
   resources :reviews, only: [:new, :create, :edit, :update, :destroy]
   resources :breweries, only: [:show]
-  resources :users
+  resources :users do
+    resources :user_breweries, only: [:new, :create, :destroy, :show]
+  end
+
+
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
