77dc7151849098fbb7a423369820d9e7e5e1c90b
dcoppage86 <dakotakcoppage@me.com>
2021-01-18 22:51:35 -0700

    continuing setting RESTful routes in controllers

--- app/controllers/user_breweries_controller.rb
+++ app/controllers/user_breweries_controller.rb
@@ -1,2 +1,12 @@
 class UserBreweriesController < ApplicationController
+    
+    def create
+    end
+
+    def show
+    end
+
+    def destroy
+    end
+    
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
 
   resources :user_breweries, only: [:create, :destroy, :show]
-  resources :reviews, only: [:create, :edit, :update, :destroy]
-  resources :breweries
+  resources :reviews, only: [:new, :create, :edit, :update, :destroy]
+  resources :breweries, only: [:show]
   resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
