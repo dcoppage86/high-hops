226d70c17c1535eea03da5915c43690b60c0fd2f
dcoppage86 <dakotakcoppage@me.com>
2021-01-18 22:07:21 -0700

    add assocations to brewery

--- app/models/brewery.rb
+++ app/models/brewery.rb
@@ -1,2 +1,6 @@
 class Brewery < ApplicationRecord
+    has_many :reviews, foreign_key: :reader_id
+    has_many :writers, through: :reviews, source: :writer
+    has_many :user_breweries
+    has_many :users, through: :user_breweries
 end
